da93ad3caf9dbd1bf0989065464a1b048abbb33c
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-10-22 10:59:59 -0600

    Resolved more conflicts

--- use-my-tech-stuff/src/index.js
+++ use-my-tech-stuff/src/index.js
@@ -3,16 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-<<<<<<< HEAD
-import { BrowserRouter as Router } from 'react-router-dom';
-
-ReactDOM.render(
-    <Router>
-      <App />
-    </Router>,
-    document.getElementById("root")
-  );
-=======
 import { BrowserRouter as Router, withRouter } from "react-router-dom";
 
 const AppWithRouter = withRouter(App);
@@ -23,5 +13,4 @@ ReactDOM.render(
     </Router>
     , document.getElementById('root'));
 
->>>>>>> 0dfb9e5336b3d6865c357c4371fbd81bc52db341
 
